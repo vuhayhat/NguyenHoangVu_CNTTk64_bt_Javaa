8af87f0219b47e7c4f55c3717a3283fc536d46ba
vuhayhat <vuhayhatmtp@gmail.com>
2024-03-13 00:58:19 +0700

    Add files via upload

--- src/Lienquantaphop_Triangle/Point.java
+++ src/Lienquantaphop_Triangle/Point.java
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-package Lienquantaphop_Triangle;public class Poin {
-=======
+
 package Lienquantaphop_Triangle;
 
 public class Point {
@@ -32,5 +30,4 @@ public class Point {
     public String toString() {
         return "(" + x + ", " + y + ")";
     }
->>>>>>> d06bbbf (Add files via upload)
 }
